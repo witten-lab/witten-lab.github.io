1e74f43414148dd30b2e447580a889c08a811281
Witten Lab <jamie@wittenlab.com>
2020-06-24 08:53:24 +0900

    Update

--- _site/js/footer.js
+++
@@ -1,4 +0,0 @@
-$(function () {
-    document.getElementById('en').setAttribute('href', window.location.pathname.replace("/ko", ""));
-    document.getElementById('ko').setAttribute('href', "/ko" + window.location.pathname.replace("/ko", ""));
-});
--- _site/js/locale-date.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    var lang = "en-US";
-    if (window.location.pathname.indexOf("/ko") > -1) lang = "ko-KR";
-    var options = { year: 'numeric', month: 'short', day: 'numeric'};
-    var releaseDateArray = document.getElementsByName('locale-date');
-    var i = releaseDateArray.length;
-    while (i--) releaseDateArray[i].innerHTML = new Date(releaseDateArray[i].innerHTML).toLocaleDateString(lang, options);
-});
