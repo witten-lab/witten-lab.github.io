404987421a01a1aad0b2147d4e7f7bcf740f3ea7
Witten Lab <jamie@wittenlab.com>
2020-07-21 19:11:02 +0900

    Update

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,12 +1,16 @@
 window.onload = function () {
-    if (window.location.hash) animateTo($(window.location.hash));
+    if (window.location.hash) scrollTo(window.location.hash);
 
     const sidebarItems = $(".sidebar a");
     sidebarItems.on("click", function (e) {
-        $("html").animate({
-            scrollTop: $($(this).attr("href")).offset().top
-        }, 1000, "easeInOutExpo");
+        scrollTo($(this).attr("href"));
     });
+
+    function scrollTo(target) {
+        $("html").animate({
+            scrollTop: $(target).offset().top
+        }, 1000, "easeInOutExpo");
+    }
 
     if (matchMedia("screen and (min-width: 1024px)").matches) {
         const anchors = gsap.utils.toArray(".anchor");
@@ -32,7 +36,6 @@ window.onload = function () {
         });
 
         function setAnchor(newAnchor) {
-            console.log(newAnchor);
             if (newAnchor !== currentAnchor) {
                 $(sidebarItems[anchors.indexOf(currentAnchor)]).removeClass('active');
                 $(sidebarItems[anchors.indexOf(newAnchor)]).addClass('active');
@@ -41,25 +44,3 @@ window.onload = function () {
         }
     }
 }
-
-/*
-$(function () {
-    //Animate with anchor
-    $('a[href*=\\#]:not([href=\\#])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            animateTo($(this.hash));
-        }
-    });
-
-    var offsetTop = $('.navbar').height() - 24;
-    function animateTo(target) {
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html,body').animate({
-                scrollTop: target.offset().top - offsetTop
-            }, 500);
-            return false;
-        }
-    }
-});
-*/
