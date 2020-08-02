558b4132cbd5d1baab085620d72be8ad73459e08
Witten Lab <jamie@wittenlab.com>
2020-08-02 17:49:41 +0900

    Update

--- js/custom.js
+++ js/custom.js
@@ -62,8 +62,65 @@ ga('send', 'pageview');
     })
 }(jQuery, window);
 
-window.onload = function () {
+window.onload = function() {
+
     $('#nav-icon').click(function () {
         $(this).toggleClass('open');
     });
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(".sub-header", {
+        scrollTrigger: {
+            trigger: ".sub-header",
+            start: "top",
+            end: $(".footer-area").position().top + "px",
+            pin: true,
+            pinSpacing: false,
+        },
+    });
+
+    if (window.location.hash) scrollTo(window.location.hash);
+
+    const sidebarItems = $(".sidebar a");
+    sidebarItems.on("click", function (e) {
+        scrollTo($(this).attr("href"));
+    });
+
+    function scrollTo(target) {
+        $("html").animate({
+            scrollTop: $(target).offset().top - 32
+        }, 1000, "easeInOutExpo");
+    }
+
+    if (matchMedia("screen and (min-width: 1024px)").matches) {
+        gsap.to(".sidebar", {
+            scrollTrigger: {
+                trigger: ".sidebar",
+                start: (-$(".navbar").outerHeight(true) - 32) + "px",
+                end: $(".content").height() - $(".navbar-header").height() - $(".sidebar").outerHeight(true) + "px",
+                pin: true,
+                pinSpacing: false,
+            },
+        });
+
+        const anchors = gsap.utils.toArray(".anchor");
+        let currentAnchor;
+
+        anchors.forEach((anchor, i) => {
+            ScrollTrigger.create({
+                trigger: anchor,
+                start: (-$(".navbar").outerHeight(true) - 32) + "px",
+                onToggle: self => self.isActive && setAnchor(anchor),
+            });
+        });
+
+        function setAnchor(newAnchor) {
+            if (newAnchor !== currentAnchor) {
+                $(sidebarItems[anchors.indexOf(currentAnchor)]).removeClass('active');
+                $(sidebarItems[anchors.indexOf(newAnchor)]).addClass('active');
+                currentAnchor = newAnchor;
+            }
+        }
+    }
 }
